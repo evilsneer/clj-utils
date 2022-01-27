8bf56199647e706b0b49da301832566ef8db3d6e
vshishkov <vshishkov@amberdata.ru>
2022-01-27 15:15:57 +0300

    git commit

--- project.clj
+++ project.clj
@@ -9,17 +9,17 @@
             [lein-ancient "0.6.15"]
             [lein-changelog "0.3.2"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.3"]]}}
-  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org/repo"}]
+  :deploy-repositories [["releases" {:sign-releases false :url "https://clojars.org/repo"}]
                         ["snapshots" {:sign-releases false :url "https://clojars.org/repo"}]]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[emptyone\\\\/utils \"[0-9.]*\"\\\\]/[emptyone\\\\/utils \"${:version}\"]/" "README.md"]
-            "upg" ["ancient" "upgrade" ":interactive" ":no-tests" ":check-clojure"]}
-  :release-tasks [["shell" "git" "diff" "--exit-code"]
-                  ["vcs" "assert-committed"]
-                  ["change" "version" "leiningen.release/bump-version"]
-                  ["change" "version" "leiningen.release/bump-version" "release"]
-                  ["changelog" "release"]
-                  #_["update-readme-version"]
-                  ["vcs" "commit"]
-                  ["vcs" "tag" "--no-sign"]
-                  ["deploy"]
-                  ["vcs" "push"]])
+            "upg"                   ["ancient" "upgrade" ":interactive" ":no-tests" ":check-clojure"]}
+  :release-tasks ^:replace [["shell" "git" "diff" "--exit-code"]
+                            ["vcs" "assert-committed"]
+                            ["change" "version" "leiningen.release/bump-version"]
+                            ["change" "version" "leiningen.release/bump-version" "release"]
+                            ["changelog" "release"]
+                            #_["update-readme-version"]
+                            ["vcs" "commit"]
+                            ["vcs" "tag" "--no-sign"]
+                            ["deploy"]
+                            ["vcs" "push"]])
