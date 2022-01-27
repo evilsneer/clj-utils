3c4d57a1891b89b22f309d8a8f56bda794decccd
vshishkov <vshishkov@amberdata.ru>
2022-01-27 15:14:04 +0300

    Version 1.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject emptyone/utils "1.0.1"
+(defproject emptyone/utils "1.0.2"
   :description "Clojure useful utils"
   :url "https://github.com/evilsneer/clj-utils"
   :license {:name "Eclipse Public License"
