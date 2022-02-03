fe1523a3efd8e1c2ab8365f7a735178bcbf5fe22
vshishkov <vshishkov@amberdata.ru>
2022-02-03 14:11:19 +0300

    add try-times

--- src/emptyone_utils/core.clj
+++ src/emptyone_utils/core.clj
@@ -133,6 +133,7 @@
     (map (fn [[k v]] [k (first v)]))
     (into {})))
 
+;; stolen from https://stackoverflow.com/questions/1879885/clojure-how-to-to-recur-upon-exception
 (defn try-times*
   "Executes thunk. If an exception is thrown, will retry. At most n retries
   are done. If still some exception is thrown it is bubbled upwards in
