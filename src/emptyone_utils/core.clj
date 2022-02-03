a7dcc5cd56c281a585bcb75ade614f3594f1a178
vshishkov <vshishkov@amberdata.ru>
2022-02-03 14:10:05 +0300

    add try-times

--- src/emptyone_utils/core.clj
+++ src/emptyone_utils/core.clj
@@ -132,3 +132,24 @@
     (group-by by)
     (map (fn [[k v]] [k (first v)]))
     (into {})))
+
+(defn try-times*
+  "Executes thunk. If an exception is thrown, will retry. At most n retries
+  are done. If still some exception is thrown it is bubbled upwards in
+  the call chain."
+  [n thunk]
+  (loop [n n]
+    (if-let [result (try
+                      [(thunk)]
+                      (catch Exception e
+                        (when (zero? n)
+                          (throw e))))]
+      (result 0)
+      (recur (dec n)))))
+
+(defmacro try-times
+  "Executes body. If an exception is thrown, will retry. At most n retries
+  are done. If still some exception is thrown it is bubbled upwards in
+  the call chain."
+  [n & body]
+  `(try-times* ~n (fn [] ~@body)))
